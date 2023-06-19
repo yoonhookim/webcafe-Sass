acd92990c17104380f878198f6f9dfd1eea17c8c
seulbinim <seulbinim@gmail.com>
2023-06-19 16:46:34 +0900

    postcss-preset-env 삭제

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
-const postcss = require('postcss');
-const postcssPresetEnv = require('postcss-preset-env');
+const postcss = require("postcss");
 
 module.exports = {
   plugins: [
-    postcss([
-      require('cssnano'),
-      require('postcss-combine-media-query')
-    ]),
-    postcssPresetEnv({
-      stage: 2,
-      minimumVendorImplementations: 2,
-      autoprefixer: { grid: true },
-      browsers: 'last 2 versions'
-    }),
-
-  ]
-}
+    postcss([require("cssnano"), require("postcss-combine-media-query")]),
+  ],
+};
